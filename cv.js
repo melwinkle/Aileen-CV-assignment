31b8e8b7ef13906841b1eb2de45c24c1471c111c
melwinkle <50456098+melwinkle@users.noreply.github.com>
2020-09-19 04:46:22 +0000

    updated

--- cv.js
+++ cv.js
@@ -1,53 +1,24 @@
-import './maincs.css';
+function passvalue(){
+    let fname=document.getElementById("f_name").value;
+    let mname=document.getElementById("m_name").value;
+    let lname=document.getElementById("l_name").value;
+    let address=document.getElementById("myaddress").value;
+    let mygen=document.getElementById("gender").value;
+    let email=document.getElementById("mymail").value;
+    let num=document.getElementById("mynumber").value;
 
 
 
-
-function onSubmit(event) {
-    event.preventDefault(); 
-    //const fname= form.elements["first"].value;
-    //const mname = form.elements["middle"].value;
-    //const lname = form.elements["last"].value;
-   // const gender = form.elements["gender"].value;
-   // const email = form.elements["email"].value;
-    //const number = form.elements["phone"].value;
-   // const address = form.elements["address"].value;
-   
-   const f_name= document.getElementById("fname").value;
-   const m_name= document.getElementById("mname").value;
-   const l_name= document.getElementById("lname").value;
-   const gender= document.getElementById("gender").value;
-   const mail= document.getElementById("email").value;
-   const phone= document.getElementById("number").value;
-   const postal= document.getElementById("address").value;
-
-
-
-   localStorage.setItem("First",f_name);
-   localStorage.setItem("Middle",m_name);
-   localStorage.setItem("Last",l_name);
-   localStorage.setItem("Gender",gender);
-   localStorage.setItem("Email",mail);
-   localStorage.setItem("Number",phone);
-   localStorage.setItem("Address",postal);
-   return ;
-
-}
-
-    let fullname=localStorage.getItem("First")+ " "+ localStorage.getItem("Middle") + " " +localStorage.getItem("Last");
-
-   document.getElementById("first-res").innerHTML=fullname;
-   document.getElementById("gender-res").innerHTML=localStorage.getItem("Gender");
-   document.getElementById("address-res").innerHTML=localStorage.getItem("Address");
-   document.getElementById("number-res").innerHTML=localStorage.getItem("Number");
-   document.getElementById("mail-res").innerHTML=localStorage.getItem("Email");
-
-   
-
-   
-
-  
+    sessionStorage.setItem("first",fname);
+    sessionStorage.setItem("middle",mname);
+    sessionStorage.setItem("last",lname);
+    sessionStorage.setItem("gender",mygen);
+    sessionStorage.setItem("address",address);
+    sessionStorage.setItem("number",num);
+    sessionStorage.setItem("email",email);
     
+    return false;
+
 }
 
 
